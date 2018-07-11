3ffbf635d7a95edbda6f589a8fde62afdcadaf65
Amélie Barré <amliebarre@gmail.com>
2018-07-11 14:08:50 +0200

    install packery and images and set packery in the homepage

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,35 +1,50 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit, AfterViewInit, ViewChild} from '@angular/core';
 import * as _ from 'lodash';
+import Packery from 'packery';
 
 import {ArtistsService} from '../../shared/services/artists.service';
 import {PaintingsService} from '../../shared/services/paintings.service';
-import {Artist} from '../../shared/models/artist';
-import {Painting} from '../../shared/models/painting';
 import {MuseumsService} from '../../shared/services/museums.service';
 import {CitiesService} from '../../shared/services/cities.service';
+
+import {Artist} from '../../shared/models/artist';
+import {Painting} from '../../shared/models/painting';
 import {City} from '../../shared/models/city';
+import {Museum} from '../../shared/models/museum';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss']
 })
-export class HomeComponent implements OnInit {
+export class HomeComponent implements OnInit, AfterViewInit {
+  @ViewChild('grid') grid;
 
   artists: Artist[] = [];
   paintings: Painting[] = [];
   city: City;
+  museums: Museum[] = [];
 
   constructor(
     private artistService: ArtistsService,
     private paintingService: PaintingsService,
-    private cityService: CitiesService
+    private cityService: CitiesService,
+    private museumService: MuseumsService
   ) { }
 
   ngOnInit() {
     this.getArtists();
     this.getPaintings();
     this.getRandomCity();
+    this.getMuseums();
+  }
+
+  ngAfterViewInit() {
+    /*let packery = new Packery(this.grid.nativeElement, {
+      itemSelector: '.grid-item'
+    });*/
+
+    console.log(this.grid);
   }
 
   getArtists() {
@@ -53,8 +68,14 @@ export class HomeComponent implements OnInit {
   getRandomCity() {
    this.cityService.getCities().subscribe((cities: City[]) => {
      this.city = cities[Math.floor(Math.random() * cities.length)];
-     console.log(this.city);
    });
+  }
+
+  getMuseums() {
+    this.museumService.getMuseums().subscribe((museums: Museum[]) => {
+      this.museums = museums;
+      console.log(this.museums);
+    });
   }
 
 }
--- src/app/shared/services/museums.service.ts
+++ src/app/shared/services/museums.service.ts
@@ -3,6 +3,8 @@ import {environment} from '../../../environments/environment';
 import {HttpClient} from '@angular/common/http';
 import {Observable} from 'rxjs/internal/Observable';
 
+import {Museum} from '../models/museum';
+
 @Injectable({
   providedIn: 'root'
 })
@@ -14,4 +16,7 @@ export class MuseumsService {
     private httpClient: HttpClient
   ) { }
 
+  getMuseums(): Observable<Museum[]> {
+    return this.httpClient.get<Museum[]>(this.apiUrl + '/museums');
+  }
 }
