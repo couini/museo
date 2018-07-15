15a13c2925b135ef84c5ad9ee523b78e91289529
Amélie Barré <amliebarre@gmail.com>
2018-07-15 11:10:06 +0200

    create artist module and artist-preview-component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,12 +2,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { HttpClientModule } from '@angular/common/http';
-
 import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { HomeComponent } from './components/home/home.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NgxMasonryModule } from 'ngx-masonry';
+import { ArtistModule } from './modules/artist/artist.module';
+
+import { AppComponent } from './app.component';
+import { HomeComponent } from './components/home/home.component';
+
 
 @NgModule({
   declarations: [
@@ -20,7 +22,8 @@ import { NgxMasonryModule } from 'ngx-masonry';
     NgbModule.forRoot(),
     HttpClientModule,
     BrowserAnimationsModule,
-    NgxMasonryModule
+    NgxMasonryModule,
+    ArtistModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/modules/artist/artist.module.ts
@@ -0,0 +1,11 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { ArtistPreviewComponent } from './components/artist-preview/artist-preview.component';
+
+@NgModule({
+  imports: [
+    CommonModule
+  ],
+  declarations: [ArtistPreviewComponent]
+})
+export class ArtistModule { }
