551d57c5a86a3ff0943a7b0be883f0a37212365d
Amélie Barré <amliebarre@gmail.com>
2018-07-15 18:10:45 +0200

    get artist details

--- src/app/modules/artist/components/artist-preview/artist-preview.component.ts
+++ src/app/modules/artist/components/artist-preview/artist-preview.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+
+import { ArtistsService } from '../../../../shared/services/artists.service';
+import { Artist } from '../../../../shared/models/artist';
 
 @Component({
   selector: 'app-artist-preview',
@@ -7,9 +11,27 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ArtistPreviewComponent implements OnInit {
 
-  constructor() { }
+  artistId: number;
+  artist: Artist;
+
+  constructor(
+    private route: ActivatedRoute,
+    private artistService: ArtistsService
+  ) { }
 
   ngOnInit() {
+    this.route.params.subscribe(params => {
+      this.artistId = params['id'];
+    });
+
+    this.getArtist();
+  }
+
+  getArtist() {
+    this.artistService.getArtist(this.artistId).subscribe((artist: Artist) => {
+      this.artist = artist;
+      console.log(artist);
+    });
   }
 
 }
