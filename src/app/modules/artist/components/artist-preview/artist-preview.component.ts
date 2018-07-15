9aad927d2f4d7437a20cba98c3b532e9fa856e84
couini <amliebarre@gmail.com>
2018-07-15 10:31:35 +0200

    import artist module in app module

---
+++ src/app/modules/artist/components/artist-preview/artist-preview.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-artist-preview',
+  templateUrl: './artist-preview.component.html',
+  styleUrls: ['./artist-preview.component.scss']
+})
+export class ArtistPreviewComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
