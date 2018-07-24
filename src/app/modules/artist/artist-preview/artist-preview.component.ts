9e235195c6bc840de83eedcf405dd685b68e61cb
Amélie Barré <amliebarre@gmail.com>
2018-07-24 13:36:01 +0200

    create movements module

---
+++ src/app/modules/artist/artist-preview/artist-preview.component.ts
@@ -0,0 +1,43 @@
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { NgxMasonryOptions as MasonryOptions } from 'ngx-masonry';
+
+import { ArtistsService } from '../../../shared/services/artists.service';
+import { Artist } from '../../../shared/models/artist';
+
+@Component({
+  selector: 'app-artist-preview',
+  templateUrl: './artist-preview.component.html',
+  styleUrls: ['./artist-preview.component.scss']
+})
+export class ArtistPreviewComponent implements OnInit {
+
+  artistId: number;
+  artist: Artist;
+
+  public options: MasonryOptions = {
+    transitionDuration: '0.8s',
+    gutter: 15
+  };
+
+  constructor(
+    private route: ActivatedRoute,
+    private artistService: ArtistsService
+  ) { }
+
+  ngOnInit() {
+    this.route.params.subscribe(params => {
+      this.artistId = params['id'];
+    });
+
+    this.getArtist();
+  }
+
+  getArtist() {
+    this.artistService.getArtist(this.artistId).subscribe((artist: Artist) => {
+      this.artist = artist;
+      console.log(artist);
+    });
+  }
+
+}
