652fd370eb0da6a97724290ba042e14283491e1b
Amélie Barré <amliebarre@gmail.com>
2018-07-26 09:39:07 +0200

    create movement details component

--- src/app/modules/movement/movement.module.ts
+++ src/app/modules/movement/movement.module.ts
@@ -2,11 +2,12 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { MovementsComponent } from './movements/movements.component';
+import { MovementDetailsComponent } from './movement-details/movement-details.component';
 
 @NgModule({
   imports: [
     CommonModule
   ],
-  declarations: [MovementsComponent]
+  declarations: [MovementsComponent, MovementDetailsComponent]
 })
 export class MovementModule { }
